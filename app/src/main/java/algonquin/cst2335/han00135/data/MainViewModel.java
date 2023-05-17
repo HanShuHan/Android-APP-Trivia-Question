bdee43e5eca64ece9fc3cf1eb66be58791a3db65
Shu Han Han <han00135@algonquinlive.com>
2023-05-17 11:23:35 -0400

    7th Commit: 1. Added MainViewModel 2. Added responsive TextView generator on "Rotation" and "Click" events
    Signed-off-by: Shu Han Han <han00135@algonquinlive.com>

---
+++ app/src/main/java/algonquin/cst2335/han00135/data/MainViewModel.java
@@ -0,0 +1,8 @@
+package algonquin.cst2335.han00135.data;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    public MutableLiveData<String> editStr = new MutableLiveData<>();
+}
--- app/src/main/java/algonquin/cst2335/han00135/ui/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/han00135/ui/MainActivity.java
@@ -1,4 +1,4 @@
-package algonquin.cst2335.han00135;
+package algonquin.cst2335.han00135.ui;
 
 import android.os.Bundle;
 import android.widget.Button;
@@ -6,12 +6,15 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
+import algonquin.cst2335.han00135.data.MainViewModel;
 import algonquin.cst2335.han00135.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding mainBinding;
+    private MainViewModel mainModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,17 +22,24 @@ public class MainActivity extends AppCompatActivity {
 
         // View Binding
         mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(mainBinding.getRoot());
-
         // View Objects
         final TextView myTextView = mainBinding.myTextView;
         final Button myButton = mainBinding.myButton;
         final EditText myEditText = mainBinding.myEditText;
+        // Sets ContentView
+        setContentView(mainBinding.getRoot());
 
+        // Gets View Model
+        mainModel = new ViewModelProvider(this).get(MainViewModel.class);
+
+        // Sets TextView content every time generated
+        myTextView.setText("Your edit text has: " + mainModel.editStr.getValue());
         // Sets myButton OnClickListener
         myButton.setOnClickListener(v -> {
-            String editStr = myEditText.getText().toString();
-            myTextView.setText("Your edit text has: " + editStr);
+            String editText = myEditText.getText().toString();
+            mainModel.editStr.postValue(editText);
         });
+        // When the data editStr is changed...
+        mainModel.editStr.observe(this, s -> myTextView.setText("Your edit text has: " + s));
     }
 }
