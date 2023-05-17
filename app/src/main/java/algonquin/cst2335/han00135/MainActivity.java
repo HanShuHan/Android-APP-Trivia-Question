8a063c55e9e8a887f551b8ecc1b1852f071e56e7
Shu Han Han <han00135@algonquinlive.com>
2023-05-17 10:02:17 -0400

    6th Commit: 1. Added EditText 2. Added View Ids 3. Added View Binding
    Signed-off-by: Shu Han Han <han00135@algonquinlive.com>

--- app/src/main/java/algonquin/cst2335/han00135/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/han00135/MainActivity.java
@@ -1,14 +1,35 @@
 package algonquin.cst2335.han00135;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import algonquin.cst2335.han00135.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding mainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // View Binding
+        mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mainBinding.getRoot());
+
+        // View Objects
+        final TextView myTextView = mainBinding.myTextView;
+        final Button myButton = mainBinding.myButton;
+        final EditText myEditText = mainBinding.myEditText;
+
+        // Sets myButton OnClickListener
+        myButton.setOnClickListener(v -> {
+            String editStr = myEditText.getText().toString();
+            myTextView.setText("Your edit text has: " + editStr);
+        });
     }
 }
