75b78d8b9796e1070ae04156e04f54fb8551575f
Shu Han Han <han00135@algonquinlive.com>
2023-05-30 22:43:10 -0400

    Fourth Commit: 1. Added Log.w() in MainActivity.java 2. Added functional login page in MainActivity.java 3. Added dial functionality in SecondActivity.java 4. Added take a photo functionality in SecondActivity.java
    Signed-off-by: Shu Han Han <han00135@algonquinlive.com>

--- app/src/main/java/algonquin/cst2335/han00135/SecondActivity.java
+++ app/src/main/java/algonquin/cst2335/han00135/SecondActivity.java
@@ -1,12 +1,18 @@
 package algonquin.cst2335.han00135;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
@@ -23,12 +29,34 @@ public class SecondActivity extends AppCompatActivity {
         TextView welcomeTextView = findViewById(R.id.welcomeMsg);
         Button callBtn = findViewById(R.id.callBtn);
         EditText phoneNum = findViewById(R.id.phoneNum);
+        ImageView cameraImg = findViewById(R.id.cameraImg);
+        Button chgPicBtn = findViewById(R.id.chgPicBtn);
 
         welcomeTextView.setText("Welcome Back " + emailAddress);
         callBtn.setOnClickListener(v -> {
             Intent call = new Intent(Intent.ACTION_DIAL);
             call.setData(Uri.parse("tel:" + phoneNum.getText().toString()));
             startActivity(call);
+        });
+
+        cameraImg.setOnClickListener(v -> {
+            Intent takePhoto = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivity(takePhoto);
+        });
+
+        // Change picture button onclick: ActivityResultLauncher
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        chgPicBtn.setOnClickListener(v -> {
+            ActivityResultLauncher<Intent> cameraResult = registerForActivityResult(
+                    new ActivityResultContracts.StartActivityForResult(),
+                    result -> {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Intent data = result.getData();
+                            Bitmap bitmap = data.getParcelableExtra("data");
+                            cameraImg.setImageBitmap(bitmap);
+                        }
+                    });
+            cameraResult.launch(cameraIntent);
         });
     }
 }
