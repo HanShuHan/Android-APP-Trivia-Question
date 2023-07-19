fc74fc6d9d5b899b0542afd014775d078122cbc2
Shu Han Han <han00135@algonquinlive.com>
2023-07-19 14:52:37 -0400

    9th Commit: 1. Added Weather Request API
    Signed-off-by: Shu Han Han <han00135@algonquinlive.com>

--- app/src/androidTest/java/algonquin/cst2335/han00135/MainActivityTest.java
+++ app/src/androidTest/java/algonquin/cst2335/han00135/MainActivityTest.java
@@ -13,11 +13,7 @@ package algonquin.cst2335.han00135;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static androidx.test.espresso.action.ViewActions.replaceText;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
@@ -51,78 +47,14 @@ public class MainActivityTest {
      */
     @Test
     public void passwordMeetsRequirementTest() {
-        ViewInteraction passwordEditText = onView(withId(R.id.password));
-        passwordEditText.perform(replaceText("1aD#"), closeSoftKeyboard());
+//        ViewInteraction passwordEditText = onView(withId(R.id.password));
+//        passwordEditText.perform(replaceText("1aD#"), closeSoftKeyboard());
 
         // Login button
-        ViewInteraction loginButton = onView(withId(R.id.loginButton));
+        ViewInteraction loginButton = onView(withId(R.id.get_forecast_button));
         loginButton.perform(click());
 
-        ViewInteraction passwordTextView = onView(withId(R.id.passwordMessage));
-        passwordTextView.check(matches(withText("Your password meets the requirements!")));
-    }
-
-    /**
-     * Tests when the password is missing a uppercase letter.
-     */
-    @Test
-    public void findPasswordMissingUppercaseTest() {
-        ViewInteraction passwordEditText = onView(withId(R.id.password));
-        passwordEditText.perform(replaceText("1a?"), closeSoftKeyboard());
-
-        // Login button
-        ViewInteraction loginButton = onView(withId(R.id.loginButton));
-        loginButton.perform(click());
-
-        ViewInteraction passwordTextView = onView(withId(R.id.passwordMessage));
-        passwordTextView.check(matches(withText("You shall not pass")));
-    }
-
-    /**
-     * Tests when the password is missing a lowercase letter.
-     */
-    @Test
-    public void findPasswordMissingLowercaseTest() {
-        ViewInteraction passwordEditText = onView(withId(R.id.password));
-        passwordEditText.perform(replaceText("1A!"), closeSoftKeyboard());
-
-        // Login button
-        ViewInteraction loginButton = onView(withId(R.id.loginButton));
-        loginButton.perform(click());
-
-        ViewInteraction passwordTextView = onView(withId(R.id.passwordMessage));
-        passwordTextView.check(matches(withText("You shall not pass")));
-    }
-
-    /**
-     * Tests when the password is missing a number letter.
-     */
-    @Test
-    public void findPasswordMissingNumberTest() {
-        ViewInteraction passwordEditText = onView(withId(R.id.password));
-        passwordEditText.perform(replaceText("aC@"), closeSoftKeyboard());
-
-        // Login button
-        ViewInteraction loginButton = onView(withId(R.id.loginButton));
-        loginButton.perform(click());
-
-        ViewInteraction passwordTextView = onView(withId(R.id.passwordMessage));
-        passwordTextView.check(matches(withText("You shall not pass")));
-    }
-
-    /**
-     * Tests when the password is missing a special character.
-     */
-    @Test
-    public void findPasswordMissingSpecialCharacterTest() {
-        ViewInteraction passwordEditText = onView(withId(R.id.password));
-        passwordEditText.perform(replaceText("0Cd"), closeSoftKeyboard());
-
-        // Login button
-        ViewInteraction loginButton = onView(withId(R.id.loginButton));
-        loginButton.perform(click());
-
-        ViewInteraction passwordTextView = onView(withId(R.id.passwordMessage));
-        passwordTextView.check(matches(withText("You shall not pass")));
+//        ViewInteraction passwordTextView = onView(withId(R.id.passwordMessage));
+//        passwordTextView.check(matches(withText("Your password meets the requirements!")));
     }
 }
